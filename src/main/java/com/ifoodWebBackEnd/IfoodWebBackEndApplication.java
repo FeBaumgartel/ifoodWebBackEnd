91da3e50294bcc8d0d4b7a41904961896687f0ea
Felipe Baumgartel <felipe.baumgartel@gmail.com>
2024-04-20 12:06:10 -0300

    feat: commit inicial

---
+++ src/main/java/com/ifoodWebBackEnd/IfoodWebBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ifoodWebBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodWebBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodWebBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifoodWebBackEnd/IfoodWebBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifoodWebBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodWebBackEndApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
